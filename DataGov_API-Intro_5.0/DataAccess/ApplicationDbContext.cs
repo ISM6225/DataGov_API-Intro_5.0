fbdfb81464968d3149950747ff89c8d94a44477b
Srinivasa Arudra Phani Bhushan Kolla <bhushankolla@usf.edu>
2021-06-04 14:41:05 -0400

    Add project files.

---
+++ DataGov_API-Intro_5.0/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataGov_API_Intro_5._0.Models;
+
+namespace DataGov_API_Intro_5._0.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : 
+            base(options) { }
+
+        public DbSet<Parks> Parks { get; set; }
+        public DbSet<Park> Park { get; set; }
+    }
+}
